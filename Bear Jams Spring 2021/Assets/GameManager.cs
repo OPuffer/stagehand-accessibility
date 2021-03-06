8d56481647f29a39da8ba65503389e8cefb7f125
Viansa Schmulbach <ansa@berkeley.edu>
2021-03-06 09:24:54 -0800

    adding gamemanager

---
+++ Bear Jams Spring 2021/Assets/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+    public GameState gameState = new GameState();
+
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    public void LoadMain()
+    {
+        
+    }
+
+    public void LoadTutorial()
+    {
+        
+    }
+
+
+    public void LoadAssembly()
+    {
+        
+    }
+
+    public void LoadScriptWriter()
+    {
+        
+    }
+
+    public void LoadFinal()
+    {
+        
+    }
+    
+    
+}
