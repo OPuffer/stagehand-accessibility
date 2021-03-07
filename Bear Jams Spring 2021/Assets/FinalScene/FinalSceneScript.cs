3ee796054d604811a23d9500463ce400b3368de8
Nikhil Jha <hi@nikhiljha.com>
2021-03-07 11:05:06 -0800

    final scene

---
+++ Bear Jams Spring 2021/Assets/FinalScene/FinalSceneScript.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using TMPro;
+using UnityEngine;
+
+public class FinalSceneScript : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI tempTbox;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject sock in GameManager.instance.gameState.socks)
+        {
+            // TODO: Show socks somehow?
+        }
+
+        foreach (String line in GameManager.instance.gameState.script)
+        {
+            tempTbox.text = tempTbox.text + line;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
