1d5a9d0d9f47dcac201eb785fd407539b254bdbc
Nikhil Jha <hi@nikhiljha.com>
2021-03-06 19:34:41 -0800

    add madlibscene

---
+++ Bear Jams Spring 2021/Assets/MadLibScene/MadLibScript.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class MadLibScript : MonoBehaviour
+{
+    private String script;
+
+    #region Inspector Variables
+    [SerializeField]
+    private TextMeshProUGUI hintText;
+    #endregion
+
+    String LoadMadLibData()
+    {
+        // I'm so sorry.
+        List<List<String>> scripts = new List<List<string>>();
+        scripts.Add(new List<string>());
+        
+        
+        int lib = Random.Range(0, 5);
+        
+        GameManager gm = GameManager.instance;
+        //gm.gameState
+    }
+    
+    String GetCurrentHint()
+    {
+        return "";
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hint_text.text = GetCurrentHint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Bear Jams Spring 2021/Assets/MainMenu/MainMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Play Button onClick
+    public void DoPlayButton()
+    {
+        GameManager gm = GameManager.instance;
+        gm.LoadAssembly();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
